149f5a93992fdbc7176e8645a4596aaf23c00e38
Jelenkee <ichhabadhs@posteo.de>
2026-07-19 13:06:43 +0200

    feat(pi): improved wikipedia tool

--- .pi/agent/extensions/gojo/index.ts
+++ .pi/agent/extensions/gojo/index.ts
@@ -1,10 +1,13 @@
 import { Type } from "typebox";
-import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-import  { Text } from "@earendil-works/pi-tui";
-import { complete, TextContent } from "@earendil-works/pi-ai/compat";
+import type { ExtensionAPI, ExtensionContext } from "@earendil-works/pi-coding-agent";
+import { Text } from "@earendil-works/pi-tui";
+import { Api, completeSimple, Model, TextContent } from "@earendil-works/pi-ai/compat";
+import { resolve } from "path";
 
 export default function (pi: ExtensionAPI) {
   setupTools(pi);
+  setupEvents(pi);
+  setupCommands(pi);
 }
 
 function setupTools(pi: ExtensionAPI) {
@@ -13,44 +16,64 @@ function setupTools(pi: ExtensionAPI) {
   pi.registerTool({
     name: "wikipedia",
     label: "Wikipedia",
-    description: "Search for information on wikipedia. Returns information",
+    description: "Search Wikipedia and extract information relevant to a specific intent. Given article titles (can be multiple) and an detailed intent describing what information is needed, this tool finds the best-matching Wikipedia article and returns the extracted content relevant to that intent.",
     promptSnippet: "Search for information on wikipedia",
     promptGuidelines: [
-      "Use wikipedia for looking up simple facts (toxcicity of hydrazine, lifespan of ladybug, battles in world war 2, ex-partners of brad pitt) or information that is likely to be found on wikipedia.org.",
-      "Prefer wikipedia over web_search if possible."
+      "Use wikipedia tool for looking up simple facts (use case of hydrazine, lifespan of ladybug, battles in world war 2, movies of brad pitt) or information that is likely to be found on wikipedia.org.",
+      "Prefer wikipedia tool over web_search for factual, encyclopedic, or biographical questions about topics likely to have a dedicated Wikipedia article — people, places, species, historical events, concepts, organizations, etc.",
+      //TODO "Use web_search instead of wikipedia tool when the information is time-sensitive, current, or unlikely to be covered by an encyclopedia article.",
     ],
     parameters: Type.Object({
-      searchTerm: Type.String({
-        description: "Short term to search for on wikipedia. Either name of the article or a simple query",
-        examples: ["windows 11", "golden gate bridge", "tour de france 2009"],
-        minLength: 2,
-        maxLength: 80,
+      articleTitles: Type.Array(Type.String, {
+        minItems: 1,
+        maxItems: 3,
+        uniqueItems: true,
+        description: "List of wikipedia articles which may contain the requested information."
       }),
-      description: Type.String({
-        description: "Detailed description what you want to know from wikipedia.",
-        minLength: 8,
+      intent: Type.String({
+        description: "A detailed description of what information is being sought from the article. Used to locate and extract the relevant section(s) of the page rather than returning the whole article.",
+        minLength: 6,
       }),
     }),
     async execute(toolCallId, params, signal, onUpdate, ctx) {
-      const searchUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=${encodeURIComponent(params.searchTerm)}&srnamespace=0&srlimit=5&srprop=`;
-      const response = await fetchWiki(searchUrl, {
-        signal,
-        headers: {
-          "user-agent": "pi.dev/extension (baumeister@posteo.de)",
-        },
-      });
-      if (!response.ok) {
-        throw new Error("Error when searching wikipedia: " + response.status);
+      ctx.ui.notify(`Searching '${params.articleTitles}'`);
+      let pageIds: number[] = [];
+      for (const articleTitle of params.articleTitles) {
+        const searchUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=${encodeURIComponent(articleTitle as string)}&srnamespace=0&srlimit=2&srprop=`;
+        const response = await fetchWiki(searchUrl, {
+          signal,
+          headers: {
+            "user-agent": "pi.dev/extension (baumeister@posteo.de)",
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Error when searching wikipedia: " + response.status);
+        }
+        const searchBody = await response.json();
+        for (const pageId of searchBody.query.search.map((o: { pageid: number }) => o.pageid)) {
+          if (!pageIds.includes(pageId)) {
+            pageIds.push(pageId)
+          }
+        }
       }
-
-      const searchBody = await response.json();
-      let pageIds = searchBody.query.search.map((o:{ pageid: number }) => o.pageid);
-      ctx.ui.notify(`Searching '${params.searchTerm}'`);
 
       const titles = [];
 
+      let models = (process.env["GOJO_SUMMARIZE_MODELS"] ?? "")
+        .split(",")
+        .map($ => $.trim())
+        .filter(Boolean);
+      if (ctx.model) {
+        models.push(ctx.model.id);
+      }
+      const model = models.flatMap(id => ctx.modelRegistry.getAvailable().filter(mod => mod.id === id)).at(0);
+      if (model == null) {
+        throw new Error("No model found to summarize wikipedia");
+      }
+
+      const texts: string[] = [];
       for (const pageId of pageIds) {
-        const parseUrl = `https://en.wikipedia.org/w/api.php?action=parse&format=json&pageid=${pageId}&prop=wikitext`;
+        const parseUrl = `https://en.wikipedia.org/w/api.php?action=parse&format=json&pageid=${pageId}&prop=text`;
         const response = await fetchWiki(parseUrl, {
           signal,
           headers: {
@@ -64,58 +87,101 @@ function setupTools(pi: ExtensionAPI) {
         const pageTitle = parseBody.parse.title;
         ctx.ui.notify(`Fetched '${pageTitle}'`);
         titles.push(pageTitle);
-        const model = ctx.model;
-        if (model == null) {
-          throw new Error("No model found to summarize wikipedia");
-        }
-        const auth = await ctx.modelRegistry.getApiKeyAndHeaders(model);
-        if (!auth.ok || !auth.apiKey) {
-          throw new Error(auth.ok ? `No API key for ${model.provider}` : auth.error);
-        }
-        const MYSTIC = "B8lgoTPOba";
-        const wikitext = parseBody.parse.wikitext["*"] as string;
-        let minifiedText = wikitext
-          .replaceAll(/{{.*?}}/g, "")
-          .replaceAll(/<ref.*?<\/ref>/g, "")
-          .replaceAll(/\[\[File:.*?\]\]/g, "")
-          .trim();
+        const wikitext = parseBody.parse.text["*"] as string;
+        const minifiedText = wikitext
+          .slice(0, wikitext.indexOf('id="References"'))
+          .replaceAll(/<style.*?<\/style>/gs, "")
+          .replaceAll(/<\/[^>]*>/gs, "<>")
+          .replaceAll(/<[^>]*>/gs, "</>")
+          .replaceAll("&#160;", " ")
+          .trim()
+        await ctx.ui.confirm(pageTitle, `${wikitext.length} - ${minifiedText.length}`);
+        texts.push(minifiedText);
 
-        let answer = await complete(model, {
-          messages: [
-            {
-              role: "user",
-              content: `Question: ${params.description}\nExtract a detailed answer from the following text. If no good answer found, return '${MYSTIC}.'\n\n${minifiedText}`,
-              timestamp: Date.now()
-            },
-          ]
-        }, { apiKey: auth.apiKey, headers: auth.headers, env: auth.env, signal: signal });
-        if (answer.stopReason == "stop" && answer.content.length > 0) {
-          let content = answer.content.filter($ => $.type === "text").filter($ => !$.text.includes(MYSTIC)).map($ => $.text).join("\n");
-          if (content.length > 2) {
-            ctx.ui.notify("");
-            return {
-              content: [{ type: "text", text: content }],
-              details: {},
-            };
-          }
-        }
       }
+      const MYSTIC = "B8lgoTPOba";
+      ctx.ui.notify(`Summarizing '${titles.join(", ")}'`);
+      let answer = await callLLM(model, [`Question: ${params.intent}?\nExtract a detailed answer from the following text. If no good answer found, return '${MYSTIC}.'\n\n${texts.map($ => `<article>${$}</article`).join("\n\n")}`], "you are an information-extracting assistant", ctx.signal, ctx);
+      if (answer && answer.includes(MYSTIC)) {
+        answer = undefined;
+      }
+      const text = answer ? answer : `No results found. Examined pages: ${titles.join(", ")}`;
       ctx.ui.notify("");
       return {
-        content: [{ type: "text", text: `No results found. Examined pages: ${titles.join(", ")}` }],
+        content: [{ type: "text", text }],
         details: {},
       };
     },
     renderResult(result, options, theme, context) {
-      const text=(result.content[0] as TextContent).text;
-      let formattedText=options.expanded?text.slice(0,500):text.slice(0,150);
-      if (formattedText.length!=text.length){
-        formattedText+="...";
+      const text = (result.content[0] as TextContent).text;
+      let formattedText = options.expanded ? text.slice(0, 600) : text.slice(0, 250);
+      if (formattedText.length != text.length) {
+        formattedText += "...";
       }
-      
-      return new Text(theme.fg("toolOutput",formattedText), 0,0);
+
+      return new Text(theme.fg("toolOutput", formattedText), 0, 0);
     }
   });
+}
+
+function setupEvents(pi: ExtensionAPI) {
+  pi.on("before_agent_start", async (event, ctx) => {
+    return {
+      systemPrompt: `${event.systemPrompt}
+
+Important guidelines:
+- Do not create/edit files outside of ${ctx.cwd} (including sub directories) or /tmp (unless explicitly instructed to do so)
+`
+    }
+  });
+  pi.on("tool_call", async (event, ctx) => {
+    if (["write", "edit"].includes(event.toolName)) {
+      let path = null;
+      if (event.toolName === "write") {
+        path = event.input.path as string
+      } else if (event.toolName === "edit") {
+        path = event.input.path as string
+      }
+      if (!path) {
+        return { block: false }
+      }
+      const absolutePath = resolve(ctx.cwd, path);
+      if (!absolutePath.startsWith("/tmp") && !absolutePath.startsWith(ctx.cwd)) {
+        if (!ctx.hasUI || !(await ctx.ui.confirm(`Tool: ${event.toolName}`, `Allow modification of ${absolutePath}`, { signal: ctx.signal }))) {
+          return { block: true, reason: `Tool only allowed in /tmp and cwd (${ctx.cwd})` }
+        }
+      }
+    }
+  });
+}
+
+function setupCommands(pi: ExtensionAPI) {
+  pi.registerCommand("gojo:dummy", {
+    async handler(args, ctx) {
+      ctx.ui.notify("waiting")
+    }
+  })
+}
+
+async function callLLM(model: Model<Api> | string, messages: string[], systemPrompt: string | undefined, signal: AbortSignal | undefined, ctx: ExtensionContext): Promise<string | undefined> {
+  if (typeof model === "string") {
+    model = ctx.modelRegistry.getAvailable().filter(mod => mod.id === model)[0];
+  }
+  const auth = await ctx.modelRegistry.getApiKeyAndHeaders(model);
+  if (!auth.ok || !auth.apiKey) {
+    throw new Error(auth.ok ? `No API key for ${model.provider}` : auth.error);
+  }
+  const response = await completeSimple(model, {
+    messages: messages.map(mes => ({ role: "user", content: mes, timestamp: Date.now() })),
+    systemPrompt,
+  }, { apiKey: auth.apiKey, headers: auth.headers, env: auth.env, signal, reasoning: "medium" })
+  if ((response.stopReason === "stop" || response.stopReason === "length") && response.content.length > 0) {
+    return response.content
+      .filter($ => $.type === "text")
+      .map($ => $.text.trim())
+      .filter(Boolean)
+      .join("\n")
+  }
 }
 
 function createCookieFetch() {
